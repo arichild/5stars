76590e3f85cc34e6ac791fb1006855523d4fda0a
RomanPanfil <clayman.roman@gmail.com>
2024-09-13 17:28:30 +0300

    alert

--- dist/js/main.js
+++ dist/js/main.js
@@ -211,98 +211,203 @@ $( document ).ready(function() {
   }
 
   let counter = 0;
-  // добавление тренера
-  $(document).on('click', '.add-coach', function(event) {
+ // добавление тренера
+  $(document).on('click', '.add-coach', function (event) {
     event.preventDefault()
 
+
     $('.ui-select').styler('destroy');
+
 
     const originalWhiteBlock = $('.white-block').last();
     const newWhiteBlock = originalWhiteBlock.clone();
 
+
     counter++;
 
+
     // обновляем id и name
-    newWhiteBlock.find('[data-name]').each(function() {
-      const dataNameValue = $(this).attr('data-name');
+    newWhiteBlock.find('[data-name]').each(function () {
+        const dataNameValue = $(this).attr('data-name');
 
-      if ($(this).attr('id')) {
-        const newId = dataNameValue + counter;
-        $(this).attr('id', newId);
-      }
 
-      if ($(this).attr('name')) {
-        const newName = dataNameValue + counter;
-        $(this).attr('name', newName);
-      }
+        if ($(this).attr('id')) {
+            const newId = $(this).attr('id') + counter;
+            $(this).attr('id', newId);
+        }
+
+
+        if ($(this).attr('name')) {
+            if (dataNameValue.includes('t[')) {
+                const index = '[' + dataNameValue.split('t[')[1].split(']')[0] + ']';
+                let newName = dataNameValue;
+                $(this).attr('name', newName.replace('t' + index, `t[${counter + 1}]`));
+            } else {
+                const newName = dataNameValue + (counter + 1);
+                $(this).attr('name', newName);
+            }
+        }
     });
+
 
     // для валидации добавляем required и очищаем поля
-    newWhiteBlock.find('input.ui-input, select.ui-input').each(function() {
-      $(this).val('');
-      $(this).prop('required', true);
+    newWhiteBlock.find('input.ui-input, select.ui-input').each(function () {
+        $(this).val('');
+        $(this).prop('required', true);
     });
+
 
     // удаляем все добавленные блоки с группой и отображаем кнопку
     newWhiteBlock.find('.ui-btn.hidden').removeClass('hidden');
-    newWhiteBlock.find('.white-block-container.new').each(function() {
-      $(this).remove()
+    newWhiteBlock.find('.white-block-container.new').each(function () {
+        $(this).remove()
     });
+
 
     newWhiteBlock.find('.input-file input[type="file"]').val('');
     newWhiteBlock.find('.input-file .preview').attr('src', '');
     newWhiteBlock.find('.input-file span').show();
 
+
     originalWhiteBlock.after(newWhiteBlock);
+
 
     // добавляем кнопку удалить
     if (!$('.ui-btn.delete-coach').length) {
-      const deleteButton = $('<button class="ui-btn red small delete-coach">Удалить</button>');
-      newWhiteBlock.find('.white-block-header').append(deleteButton);
+        const deleteButton = $('<button class="ui-btn red small delete-coach">Удалить</button>');
+        newWhiteBlock.find('.white-block-header').append(deleteButton);
     }
+
 
     // инициализация новых календарей
     const allNewCalendar = document.querySelectorAll('input.calendar')
     setCalendar(allNewCalendar)
 
+
     // Валидация для новых полей
-    $("input[data-validation], select[data-validation]").each(function() {
-      var validationTypes = $(this).data("validation");
+    $("input[data-validation], select[data-validation]").each(function () {
+        var validationTypes = $(this).data("validation");
 
-      if(validationTypes === 'lettersonly') {
-        $(this).rules('add', {
-          lettersonly: true
-        });
-      }
 
-      if(validationTypes === 'phone') {
-        $(this).rules('add', {
-          phone: true
-        });
-      }
+        if (validationTypes === 'lettersonly') {
+            $(this).rules('add', {
+                lettersonly: true
+            });
+        }
 
-      if(validationTypes === 'number') {
-        $(this).rules('add', {
-          number: true
-        });
-      }
+
+        if (validationTypes === 'phone') {
+            $(this).rules('add', {
+                phone: true
+            });
+        }
+
+
+        if (validationTypes === 'number') {
+            $(this).rules('add', {
+                number: true
+            });
+        }
     });
+
 
     // маска для телефона
     setPhoneMask()
 
+
     // стайлер селектов
     $('.ui-select').styler({
-      // selectPlaceholder: ""
+        // selectPlaceholder: ""
     });
 
+
     // для валидации
-    $('select.ui-select').on('change', function() {
-      setTimeout(function() {
-        $('select.ui-select').trigger('refresh');
-      }, 1)
+    $('select.ui-select').on('change', function () {
+        setTimeout(function () {
+            $('select.ui-select').trigger('refresh');
+        }, 1)
     });
   });
+
+
+
+
+// добавление группы
+$(document).on('click', '.add-group', function (event) {
+  event.preventDefault();
+  $('.ui-select').styler('destroy');
+
+
+  counter++
+
+
+  const container = $(this).closest('.white-block-container')
+  console.log(container)
+  const newContainer = container.clone();
+  const deleteField = newContainer.find('.delete-column')
+
+
+  newContainer.removeClass('group').addClass('new');
+  newContainer.find('.field-delete').remove();
+
+
+  // обновляем id и name
+  newContainer.find('[data-name]').each(function () {
+      const dataNameValue = $(this).attr('data-name');
+
+
+      if ($(this).attr('id')) {
+          const newId = $(this).attr('id') + counter;
+          $(this).attr('id', newId);
+      }
+
+
+      if ($(this).attr('name')) {
+          const index = dataNameValue.split('[groups]')[1].split(']')[0] + ']';
+          let newName = dataNameValue;
+          $(this).attr('name', newName.replace('[groups]' + index, `[groups][${counter + 1}]`));
+      }
+  });
+
+
+  // для валидации добавляем required
+  newContainer.find('select.ui-input').each(function () {
+      $(this).prop('required', true);
+  });
+
+
+  // добавляем кнопку удаления
+  const btnDelete = $(`
+    <div class="ui-btn red small delete-group">Удалить</div>
+  `);
+
+
+  if (!newContainer.find('.ui-btn.delete-group').length) {
+      deleteField.append(btnDelete);
+  }
+
+
+  container.after(newContainer);
+
+
+  // скрываем кнопку
+  $(this).addClass('hidden');
+
+
+  // стайлер селектов
+  $('.ui-select').styler({
+      // selectPlaceholder: ""
+  });
+
+
+  // для валидации
+  $('select.ui-select').on('change', function () {
+      setTimeout(function () {
+          $('select.ui-select').trigger('refresh');
+      }, 1)
+  });
+});
+
 
 
   // добавление группы
@@ -536,4 +641,154 @@ $( document ).ready(function() {
     updateScrollThumb();
   })();
 
+
+  
+  $('.registr-form').on('submit', function (e) {
+    if (!$(this).valid())
+        e.preventDefault()
+ })
+ 
+ 
+ 
+ 
+ const formStar = document.querySelector('select.star-form')
+ const formStarArr = document.querySelectorAll('div.star-form .jq-selectbox__dropdown li')
+ 
+ 
+ if (formStarArr.length > 0) {
+  formStarArr.forEach(el => el.addEventListener('click', async function (e) {
+      setTimeout(async function (e) {
+        const params = new URLSearchParams({starId: formStar.value}).toString();
+        const res = await fetch(`/local/templates/5stars/ajax/get-rating.php?${params}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8'
+            },
+          }
+        )
+        const rating = await res.json()
+
+
+        if (rating.status) {
+          console.log(rating)
+        } else {
+          console.error(rating.reason)
+        }
+      }, 300)
+    }
+  ))
+ }
+ 
+ 
+ const resultNav = {
+    index: 0,
+    nav: document.querySelector('.result-nav'),
+    next: {
+      node: document.querySelector('.result-nav .next'),
+      update: async (e) => {
+        const index = resultNav.index
+        if (resultNav.index === 5) resultNav.index = 0
+        else resultNav.index++
+
+
+        const isUpdate = await resultNav.update.data()
+        console.log(isUpdate)
+
+
+        if (isUpdate)
+          resultNav.update.title()
+        else
+          resultNav.index = index
+      }
+    },
+    prev: {
+      node: document.querySelector('.result-nav .prev'),
+      update: async (e) => {
+        const index = resultNav.index
+        if (resultNav.index === 0) resultNav.index = 5
+        else resultNav.index--
+
+
+        const isUpdate = await resultNav.update.data()
+        console.log(isUpdate)
+
+
+        if (isUpdate)
+          resultNav.update.title()
+        else
+          resultNav.index = index
+      }
+    },
+    title: document.querySelector('.white-block-title'),
+    update: {
+        title: () => {
+          if (resultNav.index === 0) resultNav.title.innerText = 'все звезды'
+          else resultNav.title.innerText = `${resultNav.index} звезда`
+        },
+        data: async () => {
+          const params = new URLSearchParams({starIndex: resultNav.index}).toString();
+          const res = await fetch(`/local/templates/5stars/ajax/get-result-page.php?${params}`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+              },
+            }
+          )
+          const rating = await res.json()
+
+
+          if (rating.status) {
+            document.getElementById('result-block').innerHTML = rating.resultBlock
+            document.getElementById('rating-table').innerHTML = rating.ratingTable
+
+
+            return true
+          } else
+            return false
+        },
+    }
+ }
+
+ if(resultNav && resultNav.next.node && resultNav.prev.node) {
+  resultNav.next.addEventListener('click', e => resultNav.next.update(e))
+  resultNav.prev.addEventListener('click', e => resultNav.prev.update(e))
+ }
+
+
+ // Код, который нужно было добавить конец
+
+//  alert
+(function() {
+  function showAlert(className, content) {
+    const alert = document.querySelector('.ui-alert');
+
+    if(!alert) return;
+
+    if(className === 'success') {
+      console.log('stc')
+      alert.classList.remove('error');
+      alert.classList.add('success');
+
+      alert.textContent = content
+    }
+
+    if(className === 'error') {
+      console.log('stc')
+      alert.classList.remove('success');
+      alert.classList.add('error');
+
+      alert.textContent = content
+    }
+  }
+
+  document.querySelector('.trigger-alert.success').addEventListener('click', function() {
+      showAlert('success', 'успешная отправка успешная отправка успешная отправка успешная отправка успешная отправка успешная отправка');
+  });
+
+  document.querySelector('.trigger-alert.error').addEventListener('click', function() {
+      showAlert('error', 'неуспешная отправка');
+  });
+
+})();
+ 
 });
